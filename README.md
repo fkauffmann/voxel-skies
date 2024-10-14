f6f4fda7dfa4df333f6cc796f30f3f654bb16c7b
fkauffmann <fabrice.kauffmann@gmail.com>
2024-10-14 19:30:17 +0200

    Fixed crash when switching to fullscreen on Windows

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
-# HELICOPTER SIM
+# VOXEL SKIES
 
-## TO DO
+A small helicopter simulator heavily inspired by Comanche: Maximum Overkill, published in 1992 by Novalogic.
+
+This seminal game introduced the concept of the voxel rendering engine. 
+
+The core renderer used in this project has been developed by Stanislas Petrov and can be found here : https://github.com/StanislavPetrovV/Voxel-Space-Render
+
+For the theory and maps, I used this other repository: https://github.com/StanislavPetrovV/Voxel-Space-Render
+
+<img src="screenshot.png">
+
+## Shortcuts
+
+| Key | Function |
+|-----|----------|
+| ESC | Quit |
+| Left and Righ arrows | Roll |
+| Up and Down arrows | Pitch |
+| Z | Move Up |
+| S | Move Down |
+| Q | Slide left |
+| D | Slide right |
+| L | Load next map |
+
+## Requirements
+
+* pygame
+* numpy
+* numba
+
+## To do
 
 * [x] altitude / collision detection
 * [x] damage model
@@ -20,6 +49,6 @@
 * [ ] fuel consumption
 * [ ] infinite map
 
-## KNOWN BUGS / ISSUES
+Known bugs / issues
 
 * None
--- main.py
+++ main.py
@@ -8,9 +8,8 @@ class App:
     def __init__(self):
         pg.init()
         pg.mixer.init()        
-        self.fullscreen = False
         self.res = self.width, self.height = (640, 360)
-        self.screen = pg.display.set_mode(self.res)
+        self.screen = pg.display.set_mode(self.res, pg.SCALED | pg.RESIZABLE)
         self.clock = pg.time.Clock()
         self.player = Player()
         self.explosion = Explosion()
@@ -42,17 +41,11 @@ class App:
                     sys.exit()
                 if event.type == pg.KEYDOWN and event.key == pg.K_l:
                     self.voxel_render.change_map()
-                if event.type == pg.KEYDOWN and event.key == pg.K_F11:
-                    if self.fullscreen:
-                        self.screen = pg.display.set_mode(self.res)
-                    else:
-                        self.screen = pg.display.set_mode(self.res, pg.SCALED | pg.FULLSCREEN)
-                    self.fullscreen = not self.fullscreen
 
             #pg.mixer.music.set_volume(0.4 + (abs(self.player.speed)/7))
 
             self.clock.tick(60)
-            pg.display.set_caption(f'FPS: {int(self.clock.get_fps())} - [F11]: Fullscreen')
+            pg.display.set_caption(f'FPS: {int(self.clock.get_fps())}')
 
 if __name__ == '__main__':
     app = App()
